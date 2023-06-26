aedde0dc2b205986af2999d0c4f7ad314b44c665
MiloGrb <grabiemilo@gmail.com>
2023-06-26 16:01:46 +0200

    Generic endpoint generation

--- backend/CortexBack/urls.py
+++ backend/CortexBack/urls.py
@@ -15,18 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.apps import apps
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
-from .views import HDF5ViewSet, NeuralNetworkViewSet, LayerViewSet, NeuralNetworkConfigViewSet, CortexViewSet, WorkspaceViewSet
+from rest_framework import serializers, viewsets
+from . import models
 
 router = DefaultRouter()
-router.register(r'hdf5', HDF5ViewSet)
-router.register(r'neuralnetwork', NeuralNetworkViewSet)
-router.register(r'layer', LayerViewSet)
-router.register(r'neuralnetworkconfig', NeuralNetworkConfigViewSet)
-router.register(r'cortex', CortexViewSet)
-router.register(r'workspace', WorkspaceViewSet)
+
+for model in apps.get_models():
+    model_name = model.__name__
+    serializer = type(
+        f'{model_name}Serializer', 
+        (serializers.ModelSerializer,), 
+        {'Meta': type('Meta', (object,), {'model': model, 'fields': '__all__'})}
+    )
+
+    viewset = type(
+        f'{model_name}ViewSet',
+        (viewsets.ModelViewSet,),
+        {'queryset': model.objects.all(), 'serializer_class': serializer}
+    )
+
+    router.register(rf'{model_name.lower()}', viewset)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
