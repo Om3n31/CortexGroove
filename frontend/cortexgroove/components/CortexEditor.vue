a2273eea7fdfec1754354dff24e0c6329406a3e9
Abs0l0mb <ant1lareunion@gmail.com>
2023-09-10 21:10:44 +0200

    add network popup

--- frontend/cortexgroove/components/CortexEditor.vue
+++ frontend/cortexgroove/components/CortexEditor.vue
@@ -5,7 +5,7 @@
         <div class="w-2/12 cursor-pointer border-2 border-cortex-light-green p-2 rounded-lg" v-for="layer in cortex.metadata.layers">
             <div class="text-lg text-center py-2"> {{ layer.name }}</div>
             <div class="flex gap-2 justify-between border-b-2 border-cortex-light-green pb-2">
-                <button @click="addNetwork('')" class="bg-slate-800 hover:bg-slate-700 text-white py-1 px-1 rounded-lg w-max border-2 border-cortex-light-green cursor-pointer">+ Neural Network</button>
+                <button @click="addNetwork(layer.name)" class="bg-slate-800 hover:bg-slate-700 text-white py-1 px-1 rounded-lg w-max border-2 border-cortex-light-green cursor-pointer">+ Neural Network</button>
                 <button @click="removeLayer(layer.name)" class="bg-slate-800 hover:bg-red-700 text-white py-1 px-1 rounded-lg w-max border-2 border-red-500 cursor-pointer">Remove layer</button>
             </div>
             <div class="p-4 text-slate-500" v-if="cortex.metadata.layers.length"> Add neural networks to build your layers. Each neural network of this layers will be fully connected to all neural networks of the adjacents layers.</div>
@@ -13,11 +13,15 @@
             </div>
         </div>
     </div>
+    <AddNetworkPopup v-show="showPopup" @close="closePopup()" :layerName="layerToEdit"></AddNetworkPopup>
 </template>
 
 <script setup lang="ts">
 
     import { ref } from 'vue';
+
+    let showPopup = ref(false);
+    let layerToEdit = ref('');
 
     let cortex = ref(
         {
@@ -42,7 +46,35 @@
     }
 
     function addNetwork(layerName: string): void {
-        console.log('adding network...');
-        
+        showPopup.value = true;
+        layerToEdit.value = layerName;
     }
+    
+	function closePopup() {
+		showPopup.value = false;
+	}
+
+    /*
+	let cortexes = ref([
+		{
+			name: `cortex 1`,
+			structure: 
+			[
+				[ { name: `network 1`, data: `data 1` }, { name: `network 2`, data: `data 2` } ],
+				[ { name: `network 3`, data: `data 3` }, { name: `network 4`, data: `data 4` }, { name: `network 5`, data: `data 5` } ],
+				[ { name: `network 6`, data: `data 6` } ]
+			]
+		},
+		{
+			name: `cortex 2`,
+			structure: 
+			[
+				[ { name: `network 7`, data: `data 7` }, { name: `network 8`, data: `data 8` } ],
+				[ { name: `network 9`, data: `data 9` }, { name: `network 10`, data: `data 10` }, { name: `network 11`, data: `data 11` } ],
+				[ { name: `network 12`, data: `data 12` } ]
+			]
+		},
+	]);
+    */
+
 </script>
